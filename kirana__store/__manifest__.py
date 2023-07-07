43a2a237cafbed63b12e4cfd2a1644bf4a9b7926
gautam77 <gautam.makwana@yudiz.com>
2023-07-07 14:24:20 +0530

    odoo kirana store app add

--- kirana__store/__manifest__.py
+++ kirana__store/__manifest__.py
@@ -16,8 +16,8 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Tutorials',
+   
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
@@ -27,7 +27,8 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-    ],
+        'demo/demo.xml'
+    ],  
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
